8dbb9ef7a12a6678de34e5913210181e0b6b4511
vinay <ypvinayreddy@gmail.com>
2021-02-05 10:27:03 -0700

    adding backend data

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreens from './screens/HomeScreens';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+	return (
+		<Router>
+			<Header />
+			<main>
+				<Container>
+					<Route path="/" component={HomeScreens} exact />
+					<Route path="/product/:id" component={ProductScreen} />
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	);
+}
+
+export default App;
