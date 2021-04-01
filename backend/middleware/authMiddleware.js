b9131b20c6359328920f4029b40fbf0e4a2cd4f7
vinay <ypvinayreddy@gmail.com>
2021-04-01 10:19:47 -0700

    add authMiddleware config

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,26 +4,26 @@ import asyncHandler from 'express-async-handler';
 
 const protect = asyncHandler(async (req, res, next) => {
 	let token;
-	console.log(req.headers.authorization);
+
 	if (
 		req.headers.authorization &&
 		req.headers.authorization.startsWith('Bearer')
 	) {
 		try {
 			token = req.headers.authorization.split(' ')[1];
-			console.log('token', token);
+
 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
-			console.log('decoded', decoded);
-			req.user = await (await User.findById(decoded.id)).isSelected(
-				'-password'
-			);
+
+			req.user = await User.findById(decoded.id).select('-password');
+
 			next();
 		} catch (error) {
 			console.error(error);
 			res.status(401);
-			throw new Error('token failed');
+			throw new Error('Not authorized, token failed');
 		}
 	}
+
 	if (!token) {
 		res.status(401);
 		throw new Error('Not authorized, no token');
